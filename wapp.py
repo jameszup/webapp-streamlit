3b5fe4338223e97984ee4ba508390fe4367dec1d
james martin <belenjamesmartin2@gmail.com>
2026-03-06 10:48:35 +0800

    Initial commit

---
+++ wapp.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("My First Streamlit Web App")
+
+name = st.text_input("Enter your name")
+
+if name:
+    st.write(f"Hello {name}! 👋")
+
+number = st.slider("Pick a number", 1, 100)
+
+st.write("You selected:", number)
